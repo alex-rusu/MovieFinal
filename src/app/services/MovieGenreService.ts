8a8a9350c86c0f8f069a9bf059a1c015b9f9c9c9
Alex Rusu <alexandru.rusu@student.fairfield.edu>
2021-12-17 14:30:14 -0500

    added http to link

--- src/app/services/MovieGenreService.ts
+++ src/app/services/MovieGenreService.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class MovieGenreService {
   findAllMovieGenres = () =>
-    fetch('Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movieGenre')
+    fetch('http://Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movieGenre')
       .then(response => response.json())
 }
--- src/app/services/MovieService.ts
+++ src/app/services/MovieService.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class MovieService {
   findAllMovies = () =>
-    fetch('Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movie')
+    fetch('http://Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movie')
       .then(response => response.json())
 }
