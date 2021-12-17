1a77d35bb1d68c4d73633668244a158173d1d096
Alex Rusu <alexandru.rusu@student.fairfield.edu>
2021-12-17 14:28:53 -0500

    modified services to point to new db

--- src/app/services/MovieGenreService.ts
+++ src/app/services/MovieGenreService.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class MovieGenreService {
   findAllMovieGenres = () =>
-    fetch('http://moviefinalv2-env.eba-pmaxbhvb.us-east-2.elasticbeanstalk.com/api/movieGenre')
+    fetch('Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movieGenre')
       .then(response => response.json())
 }
--- src/app/services/MovieService.ts
+++ src/app/services/MovieService.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class MovieService {
   findAllMovies = () =>
-    fetch('http://moviefinalv2-env.eba-pmaxbhvb.us-east-2.elasticbeanstalk.com/api/movie')
+    fetch('Moviefinalv3-env.eba-wviq2tqt.us-east-2.elasticbeanstalk.com/api/movie')
       .then(response => response.json())
 }
